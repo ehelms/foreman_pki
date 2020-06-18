b10a125bd8f7089ee28bf14e2611f7e457dc7df7
Eric D. Helms <ericdhelms@gmail.com>
2020-06-18 18:04:16 +0000

    Add bundle import command

---
+++ lib/foreman_pki/command/import_command.rb
@@ -0,0 +1,17 @@
+module ForemanPki
+  module Command
+    class ImportCommand < Clamp::Command
+      parameter "bundle", "Specify the certificate bundle to import"
+
+      def execute
+        config = Config.new
+
+        build_env = ForemanPki::BuildEnvironment.new('ca')
+
+        Dir.chdir(build_env.base_dir) do
+          `/usr/bin/tar -xzf #{bundle}`
+        end
+      end
+    end
+  end
+end
